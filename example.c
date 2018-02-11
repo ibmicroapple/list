33d68336978a269d0ca3eef63c1f3622310bb3d4
ibmicroapple <ibmicroapple@gmail.com>
2018-02-11 03:31:25 +0100

    Added example

--- example.c
+++ example.c
@@ -1,4 +1,14 @@
+/* TODO: Unfinished example...
+ *
+ */
+
 #include "list.h"
+
+typedef struct
+{
+	int number;
+	char name[24];
+} CoolObject;
 
 int main()
 {
@@ -10,6 +20,42 @@ int main()
 	 */
 
 	List *myDynamicList = list_new();
+
+	/* Here we have an array of very cool objects -
+	 * These can of course be of any datatype.
+	 */
+
+	CoolObject myCoolObjects[] =
+	{
+		{.number = 1, .name = "one"},
+		{.number = 2, .name = "two"},
+		{.number = 3, .name = "three"},
+		{.number = 4, .name = "four"},
+		{.number = 5, .name = "five"},
+		{.number = 6, .name = "six"},
+		{.number = 7, .name = "seven"},
+		{.number = 8, .name = "eight"},
+		{.number = 9, .name = "nine"}
+	};
+	#define NUM_OBJECTS (sizeof(myCoolObjects) / sizeof(myCoolObjects[0]))
+
+	/* Now we'll just add every single object to the end
+	 * of our list.
+	 */
+
+	size_t i;
+
+	for(i = 0; i < NUM_OBJECTS; i++)
+	{
+		list_add_last(myDynamicList, myCoolObjects[0]);
+	}
+
+	/* Clear the list
+	 * Note: If you have dynamically allocated elements within the list
+	 * you'd have to free them first.
+	 */
+
+	list_clear(myDynamicList);
 
 	/* Free the dynamically allocated list.
 	 * You do not need to free local (stack) lists (obviously).
